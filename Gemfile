40a1d0249ac668a019ffdb13f5a43775b10f690c
Kostas Karachalios <kostas.karachalios@gmail.com>
2011-08-24 10:08:17 +0300

    fixed specs and added travic config

--- Gemfile
+++ Gemfile
@@ -5,8 +5,8 @@ source "http://rubygems.org"
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
-group :development do
-  gem "bundler", "~> 1.0.0"
-  gem "jeweler", "~> 1.5.2"
-  gem "rspec", "~> 2.5.0"
-end
+
+gem "bundler", "~> 1.0.0"
+gem "jeweler", "~> 1.5.2"
+gem "rspec", "~> 2.5.0"
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,9 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'the-perfect-gem'
+
+require File.expand_path('../../lib/little_math_pet.rb', __FILE__)
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
-RSpec.configure do |config|
-  
-end
+RSpec.configure do |config|; end
